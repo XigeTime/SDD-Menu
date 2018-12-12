3e5a84c3be26681185cb781dbd7cec3272b2d4ca
Harry K <harry.c.knowles@gmail.com>
2018-12-12 21:30:28 +0000

    Removing CSS dependencies when positioning dropdown

--- js/sdd-menu.js
+++ js/sdd-menu.js
@@ -2,35 +2,58 @@
 // Simple dropdown menu - Easy to use 
 // Designed by xigeti.me - forumonic.com
 
-function expandDDMenu() {     
+function expandDDMenu(event) {     
   var id = $(this).attr("id"), // Get elements ID.
       child = $(this).children(".dd-child"), // Find child dropdown element.
       parent = $(this).parent().hasClass("dd-menu-container"), // Check if has dd-container.
-      open = $(".dd-menu.dd-open").length; // Check if any menus are currently open.
+      open = $(".dd-menu.dd-open").length, // Check if any menus are currently open.
+      coOrdinates = document.querySelector("#" + id).getBoundingClientRect();
     
   if ( open > 0 ) {
-    collapseDDMenu() // Close open menus.
+    collapseDDMenu(event) // Close open menus.
   }
 
   if (parent) {
-    var pos = $(this).height(); // Get height of dd-menu element.
+    var height = $("#" + id).parent().height(); // Get height of parent element.
   } else {
-    var pos = $("#" + id).parent().height(); // Get height of parent element.
+    var height = coOrdinates.height; // Get height of dd-menu element.
   }
 
-  if ( $(this).hasClass("t") ) {
-    var style = { "bottom": pos,"display":"block" };
+  if ( $(this).css("position") === "relative" ) {
+    if ( $(this).hasClass("t") ) {
+      var style = { "bottom": height,"display":"block" };
+    } else {
+      var style = { "top": height,"display":"block" };
+    } // If dropdown is set to top
+    if ( $(this).hasClass("r") ) {
+      var X = { "right": 0 };
+    } else {
+      var X = { "left": 0 };
+    }
   } else {
-    var style = { "top": pos,"display":"block" };
-  } // If dropdown is set to top
+    if ( $(this).hasClass("r") ) {
+      var X = { "right": $(document).width() - coOrdinates.right };
+    } else {
+      var X = { "left": coOrdinates.left };
+    }
+    if ( $(this).hasClass("t") ) {
+      var style = { "bottom": $(document).height() - coOrdinates.top,"display":"block" };
+    } else {
+      var style = { "top": coOrdinates.top + height,"display":"block" };
+    } // If dropdown is set to top
+  }
 
   child.slideDown({
-    start: function () { $(this).css(style) }
+    start: function () { 
+      $(this).css(style);
+      $(this).css(X);
+    }
   });
 
   $(this).addClass("dd-open");
   child.addClass("o");
   $(this).one("click", collapseDDMenu); // Set next click function
+  event.preventDefault();
   event.stopPropagation();
 
   var bounding = document.querySelector(".dd-child.o").getBoundingClientRect(); // Get elements co-ordinates & check if is inside viewport
@@ -44,8 +67,9 @@ function expandDDMenu() {
   }
 };
 
-function collapseDDMenu() {
+function collapseDDMenu(event) {
   $(".dd-menu.dd-open").removeClass("dd-open").one("click", expandDDMenu).children(".dd-child").slideUp().removeClass("o");
+  event.preventDefault();
 };
 $(window).click(function() { 
   $(".dd-menu.dd-open").removeClass("dd-open").one("click", expandDDMenu).children(".dd-child").slideUp().removeClass("o"); 
